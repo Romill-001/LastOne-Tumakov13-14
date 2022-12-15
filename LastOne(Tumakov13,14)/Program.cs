37911308ada5d51673e1032422fb876a49e76967
Ilya <112769189+Romill-001@users.noreply.github.com>
2022-12-15 22:47:48 +0300

    Добавьте файлы проекта.

---
+++ LastOne(Tumakov13,14)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastOne_Tumakov13_14_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
